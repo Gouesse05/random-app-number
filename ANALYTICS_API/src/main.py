dc547ee6cb33d8401548d019f579b2b8750f8f6c
gouessego <gouesseehemie@gmail.com>
2025-12-26 19:34:58 +0100

    Update

--- ANALYTICS_API/src/main.py
+++ ANALYTICS_API/src/main.py
@@ -1,16 +1,19 @@
-
 from typing import Union
 from fastapi import FastAPI
-from fastapi import FastAPI, HTTPException
+from src.api.events import router as events_router
 
 app = FastAPI()
-@app.get("/")
 
+app.include_router(events_router, prefix="/api/events")
+
+@app.get("/")
 def read_root():
-	return {"Hello": "World"}
+    return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-
 def read_item(item_id: int, q: Union[str, None] = None):
-	return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}
 
+@app.get("/healthz")
+def read_health_api():
+    return {"status": "ok"}
