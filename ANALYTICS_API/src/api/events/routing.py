7f4fa35f6d3f9a9d7dc393b4864f2090590c6f8f
gouessego <gouesseehemie@gmail.com>
2025-12-27 13:50:55 +0100

    Update

--- ANALYTICS_API/src/api/events/routing.py
+++ ANALYTICS_API/src/api/events/routing.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
-
+from .schemas import EventSchema
 router = APIRouter()
 #/api/events/
 @router.get("/")
 def read_events():
+    # a bunch of items  in table
     return {
-        "items": [1,2,3]
+        "result": [1,2,3]
     }
+
+
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+    #single row
+    return {"id": event_id}
